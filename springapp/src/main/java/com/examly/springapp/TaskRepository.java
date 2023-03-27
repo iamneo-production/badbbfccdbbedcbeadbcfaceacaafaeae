2f2ee43ab798974a8105f39b573bf61ffde02538
root <root@badbbfccdbbedcbeadbcfaceacaafaeae-0.badbbfccdbbedcbeadbcfaceacaafaeae.che.svc.cluster.local>
2023-03-27 04:46:58 +0000

    Backup Branch before deleting

---
+++ springapp/src/main/java/com/examly/springapp/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.examly.springapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Task findByTaskHolderName(String taskHolderName);
+
+    Task saveAll(TaskController task);
+}
